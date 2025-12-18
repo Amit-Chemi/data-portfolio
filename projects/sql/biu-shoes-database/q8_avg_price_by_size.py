c1c8e6afe945fb070e3e91a693a20162b536a113
Amit-Chemi <chemiamit@gmail.com>
2025-12-18 14:30:26 +0200

    Create q8_avg_price_by_size.py

---
+++ projects/sql/biu-shoes-database/q8_avg_price_by_size.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb=mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="root",
+    database="biu_shoes",
+    port='3307',
+    )
+    cursor=mydb.cursor ()
+    # Finds price distribution of shoes for different sizes.
+    cursor.execute ("""
+    SELECT sz.european_number, sz.us_number, AVG(sh.price) AS average_price
+    FROM size AS sz
+        JOIN shoe_size AS ss ON sz.size_id = ss.size_id
+        JOIN shoe AS sh ON ss.shoe_id = sh.shoe_id
+    GROUP BY sz.european_number, sz.us_number
+    ORDER BY average_price DESC;
+    """)
+    print (', '. join (str( row ) for row in cursor . fetchall ()
+    ))
