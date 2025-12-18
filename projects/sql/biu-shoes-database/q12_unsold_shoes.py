2a67f20d56854b8d29041e8033e6f3345768ac41
Amit-Chemi <chemiamit@gmail.com>
2025-12-18 14:33:44 +0200

    Create q12_unsold_shoes.py

---
+++ projects/sql/biu-shoes-database/q12_unsold_shoes.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb=mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="root",
+    database="biu_shoes",
+    port='3307',
+    )
+    cursor=mydb.cursor ()
+    # List of shoes that have not yet been ordered by any customer.
+    cursor.execute ("""
+    SELECT DISTINCT sh.shoe_name
+    FROM shoe AS sh
+    WHERE NOT EXISTS (SELECT 1
+                      FROM order_shoe AS os
+                      WHERE os.shoe_id = sh.shoe_id);
+    """)
+    print (', '. join (str( row ) for row in cursor . fetchall ()
+    ))
