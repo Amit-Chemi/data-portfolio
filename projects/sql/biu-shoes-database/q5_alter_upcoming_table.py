e5b8294237f6edbb9cda205425c0a3a3eee69926
Amit-Chemi <chemiamit@gmail.com>
2025-12-18 14:25:32 +0200

    Create q5_alter_upcoming_table.py

---
+++ projects/sql/biu-shoes-database/q5_alter_upcoming_table.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="biu_shoes",
+    port=3307,
+)
+    cursor=mydb.cursor ()
+    # Adding Pre-order Available column into upcoming table.
+    cursor.execute ("""
+    ALTER TABLE upcoming ADD COLUMN pre_order_available BIT(1) NOT NULL DEFAULT 0;
+    """)
+    mydb.commit ()
+    cursor.close ()
+    mydb.close ()
