c8099021ef382dad917faaf15aca742af2c180f7
Amit-Chemi <chemiamit@gmail.com>
2025-12-30 15:22:39 +0200

    Update q8_avg_price_by_size.py

--- projects/sql/biu-shoes-database/q8_avg_price_by_size.py
+++ projects/sql/biu-shoes-database/q8_avg_price_by_size.py
@@ -2,11 +2,11 @@ import mysql.connector
 
 if __name__ == '__main__':
     mydb=mysql.connector.connect (
-    host="localhost",
-    user="root",
-    password="root",
-    database="biu_shoes",
-    port='3307',
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
     )
     cursor=mydb.cursor ()
     # Finds price distribution of shoes for different sizes.
