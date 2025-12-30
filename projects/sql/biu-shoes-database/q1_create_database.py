962f546075f99225677792366f00c91d5e775495
Amit-Chemi <chemiamit@gmail.com>
2025-12-30 15:15:45 +0200

    Update q1_create_database.py

--- projects/sql/biu-shoes-database/q1_create_database.py
+++ projects/sql/biu-shoes-database/q1_create_database.py
@@ -2,11 +2,11 @@ import mysql.connector
 
 if __name__ == '__main__':
     mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    port=3307,
-)
+        host="localhost",
+        user="root",
+        password="root",
+        port=3307,
+    )
     cursor=mydb.cursor ()
     # Creation of the db.
     cursor.execute ("""
