413a3a4e3c5986410d66f48b7850d5af54701b63
Amit-Chemi <chemiamit@gmail.com>
2025-12-30 15:16:17 +0200

    Update q3_insert_data.py

--- projects/sql/biu-shoes-database/q3_insert_data.py
+++ projects/sql/biu-shoes-database/q3_insert_data.py
@@ -2,12 +2,12 @@ import mysql.connector
 
 if __name__ == '__main__':
     mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="biu_shoes",
-    port=3307,
-)
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port=3307,
+    )
     cursor=mydb.cursor ()
     cursor.execute ("""
     # Inserting data into shoe table.
