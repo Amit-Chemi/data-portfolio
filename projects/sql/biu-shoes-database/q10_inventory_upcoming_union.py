2e81496e1c8c20cbe8d4b9804b7fbbaeac81ce5c
Amit-Chemi <chemiamit@gmail.com>
2025-12-30 15:23:07 +0200

    Update q10_inventory_upcoming_union.py

--- projects/sql/biu-shoes-database/q10_inventory_upcoming_union.py
+++ projects/sql/biu-shoes-database/q10_inventory_upcoming_union.py
@@ -2,11 +2,11 @@ import mysql.connector
 
 if __name__ == '__main__':
     mydb=mysql.connector.connect (
-    host="localhost",
-    user="root",
-    password="root",
-    database="biu_shoes",
-    port='3307',
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
     )
     cursor=mydb.cursor ()
     # Consolidated list of all available shoe names and upcoming collections.
